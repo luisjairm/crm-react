5bcd61a7fbf6d82ab185b0c0111226291c5db711
luisjairm <luisjairm.dev@gmail.com>
2023-04-06 12:28:37 -0600

    159. Resaltar la página actual

--- src/components/Layaout.jsx
+++ src/components/Layaout.jsx
@@ -1,6 +1,8 @@
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link, useLocation } from 'react-router-dom'
 
 const Layout = () => {
+  const location = useLocation()
+
   return (
     <div
       className='md:flex md:min-h-screen '
@@ -8,8 +10,8 @@ const Layout = () => {
       <aside className='md:w-1/4 bg-blue-900 px-5 py-10'>
         <h2 className='text-4xl font-black text-center text-white'>CRM - Clientes</h2>
         <nav className='mt-10'>
-          <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to='/'>Inicio</Link>
-          <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to='/clientes/nuevo'>Nuevo Cliente</Link>
+          <Link className={`${location.pathname === '/' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300 `} to='/'>Inicio</Link>
+          <Link className={`${location.pathname === '/clientes/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300 `} to='/clientes/nuevo'>Nuevo Cliente</Link>
         </nav>
       </aside>
       <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
